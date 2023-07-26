f6dc7d7ec1bc55cf13a5081ec0f8b3cc3cd5dd21
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-26 14:35:29 +0530

    roboshop-terraform

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "component" {}
+variable "env" {}
+variable "tags" {}
+variable "instance_type" {}
+variable "sg_subnets_cidr" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "allow_ssh_cidr" {}
