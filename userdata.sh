0514f0025b5e3265bd3320ade361a5a4b480da07
Siva Kumar <sivakumar9399959803@gmail.com>
2023-08-25 10:40:38 +0530

    roboshop-terraform

--- userdata.sh
+++ userdata.sh
@@ -2,5 +2,4 @@
 
 set-hostname -skip-apply ${component}
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/siva-devops73/roboshop-ansible main.yml -e role_name=rabbitmq -e
- env=${env}  &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/siva-devops73/roboshop-ansible main.yml -e role_name=rabbitmq -e env=${env}  &>>/opt/ansible.log
